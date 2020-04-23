7058eb32b1025c5321bb3b531f61e40b10919ab0
ailaijunxia <1016840394@qq.com>
2020-04-23 12:02:56 +0800

    修改

--- pages/Pricesearch/index.js
+++ pages/Pricesearch/index.js
@@ -6,22 +6,52 @@ Page({
    */
   data: {
     searchtext: '',
+    arr:[],
+    check_arr:[]
   },
   back() {
     wx.navigateBack({
       delta: 1
     })
   },
+  search(e){
+    let text = e.detail.value.replace(/\s*/g, "")
+    let arr = this.data.arr
+    let arr1 = []
+    for(let i = 0;i<arr.length;i++){
+      if (arr[i].handlename.indexOf(text)!=-1){
+        arr1.push(arr[i])
+      }
+    }
+    this.setData({
+      searchtext: text,
+      check_arr:arr1
+    })
+  },
   searchclick(e) {
     console.log(e)
     console.log(this.data.searchtext)
   },
-
+  gethandle() {
+    let self = this
+    wx.request({
+      url: getApp().data.APIS + '/sysset/gethandle',
+      method: 'post',
+      success: function (res) {
+        console.log(res)
+        if (res.data.info == 'ok') {
+          self.setData({
+            arr: res.data.list,
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.gethandle()
   },
 
   /**
